9835614aa50b7fb9bb030017458d347723efd5e4
AngeloRoudani <ange.rouda@gmail.com>
2023-03-16 16:05:02 +0100

    implementazione line through

--- js/main.js
+++ js/main.js
@@ -1,9 +1,43 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
-  createApp({
-    data() {
-      return {
-        message: 'Hello Vue!'
-      }
-    }
-  }).mount('#app')
+    createApp({
+            data() {
+                return {
+
+                    taskArray: [
+                        {
+                            text:"Comprare Pane",
+                            done: false,
+                        },
+                        {
+                            text:"Andare in posta",
+                            done: true,
+                        },
+                        {
+                            text:"Andare in banca",
+                            done: true,
+                        },
+                        {
+                            text:"Comprare computer nuovo",
+                            done: false,
+                        }
+                    ]
+                }
+            },
+            methods: {
+                deleteTask (index) {
+                    this.taskArray = this.taskArray.splice(index,1)
+                },
+                getDone () {
+                    if(this.taskArray.done == false) {
+                        this.taskArray.done = true;
+                        this.taskArray.text.style.textDecoration = "line-through";
+                    } else {
+                        this.taskArray.done = false
+                    }
+                },
+
+            }
+      
+        
+    }).mount('#app')
